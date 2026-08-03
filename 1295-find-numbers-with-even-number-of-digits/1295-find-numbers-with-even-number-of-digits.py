4d26e768c1d3d47f15fefc9d35df489570289de1
akhilvvs0713 <akhilvvs0713@gmail.com>
2026-08-03 20:55:54 +0530

    Time: 4 ms (15.73%), Space: 19.1 MB (92.35%) - LeetHub

---
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        c=0
+        for i in nums:
+            d=0
+            n=i
+            if n<10:
+                d=1
+                continue
+            while n>0:
+                n//=10
+                d+=1
+            if d%2==0:
+                c+=1
+        return c
