5e62d3d1c1485db1171dada9affa771356449883
akhilvvs0713 <akhilvvs0713@gmail.com>
2026-08-06 10:51:08 +0530

    Time: 0 ms (100%), Space: 12.3 MB (62.03%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -1,12 +1,17 @@
-class Solution:
-    def smallestNumber(self, n: int, t: int) -> int:
-        def DigitMultiply(num):
+class Solution(object):
+    def smallestNumber(self, n, t):
+        """
+        :type n: int
+        :type t: int
+        :rtype: int
+        """
+        def MultiplyDigit(num):
             m=1
             while m>0 and num>0:
                 m*=num%10
                 num//=10
             return m
         for i in range(n,101):
-            if DigitMultiply(i) % t==0:
+            if MultiplyDigit(i) % t == 0:
                 return i
         return n
